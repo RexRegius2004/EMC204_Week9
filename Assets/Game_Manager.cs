00911ce599f3912a04f7bd3ae7d4f70937dada1c
CIITSHSDTP00\CIIT <134357506+RexRegius2004@users.noreply.github.com>
2024-10-26 10:46:47 +0800

    V.1

---
+++ Assets/Game_Manager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using TMPro.Examples;
+
+public class Game_Manager : MonoBehaviour
+{
+    [SerializeField] public Rigidbody rb;
+    [SerializeField] public WheelJoint2D Frontwheel;
+    [SerializeField] public WheelJoint2D Backwheel;
+    [SerializeField] public float CartSpeed;
+    [SerializeField] public TextMeshProUGUI ScorePoint;
+    [SerializeField] public float coalpoint;
+    [SerializeField] public Collider2D Collider;
+    [SerializeField] public GameObject Coal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Score();
+        CoalSpawner()
+    }
+
+    void Movement()
+    {
+        float h = Input.GetAxis("Horizontal") * CartSpeed;
+
+        Frontwheel.useMotor = true;
+        var fmotor = Frontwheel.motor;
+        fmotor.motorSpeed = h;
+        Frontwheel.motor = fmotor;
+
+        Backwheel.useMotor = true;
+        var bmotor = Backwheel.motor;
+        bmotor.motorSpeed = h;
+        Backwheel.motor = bmotor;
+
+    }
+
+    void Score()
+    {
+        ScorePoint.text = coalpoint.ToString();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        coalpoint+=1;
+        Destroy(other.gameObject);
+    }
+
+    CoalSpawner
+}
